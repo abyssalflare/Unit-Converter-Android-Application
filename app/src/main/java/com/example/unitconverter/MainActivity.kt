f9dba45db59685b0a852846e9d88ddb72c61ec7e
Jeryl Goh <jerylgoh2003@gmail.com>
2024-08-03 23:02:20 +0800

    Added Spacing
    used spacer composable and padding modifier to add spacing between composables

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -22,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,10 +55,11 @@ fun UnitConverter()
     ) {
         //Within column, all the UI elements will be stacked below each other
         Text("Unit Converter");
+        Spacer(modifier = Modifier.height(15.dp));
         OutlinedTextField(value = "", onValueChange = {//insert code when value in text field changes
 
         });
-        Text("Result:");
+        Spacer(modifier = Modifier.height(15.dp));
         Row {
             //Within row, all the UI elements will be side by side
             Box{
@@ -70,6 +75,8 @@ fun UnitConverter()
                 }
             }
         }
+        Spacer(modifier = Modifier.height(15.dp));
+        Text("Result:");
     }
 }
 
