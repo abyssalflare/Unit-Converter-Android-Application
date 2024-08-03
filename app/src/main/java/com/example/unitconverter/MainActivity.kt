027ca8d873e7a44491833acacdd22b9a0e488fb5
Jeryl Goh <jerylgoh2003@gmail.com>
2024-08-03 23:22:40 +0800

    Added Dropdownmenus and dropdownmenuitems
    experimented using as mentioned above

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -12,9 +12,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -55,11 +58,11 @@ fun UnitConverter()
     ) {
         //Within column, all the UI elements will be stacked below each other
         Text("Unit Converter");
-        Spacer(modifier = Modifier.height(15.dp));
+        Spacer(modifier = Modifier.height(16.dp));
         OutlinedTextField(value = "", onValueChange = {//insert code when value in text field changes
 
         });
-        Spacer(modifier = Modifier.height(15.dp));
+        Spacer(modifier = Modifier.height(16.dp));
         Row {
             //Within row, all the UI elements will be side by side
             Box{
@@ -67,15 +70,52 @@ fun UnitConverter()
                     Text(text = "Select");
                     Icon(Icons.Default.ArrowDropDown, "Set to empty");
                 }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(
+                        text = { Text(text = "Centimeters") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Meters") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Feet") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Millimeters") },
+                        onClick = { /*TODO*/ }
+                    )
+                }
             }
+            Spacer(modifier = Modifier.width(16.dp));
             Box{
                 Button(onClick = { /*TODO*/ }) {
                     Text(text = "Select");
                     Icon(Icons.Default.ArrowDropDown, "Set to empty");
                 }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(
+                        text = { Text(text = "Centimeters") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Meters") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Feet") },
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Millimeters") },
+                        onClick = { /*TODO*/ }
+                    )
+                }
             }
         }
-        Spacer(modifier = Modifier.height(15.dp));
+        Spacer(modifier = Modifier.height(16.dp));
         Text("Result:");
     }
 }
